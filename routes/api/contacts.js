fe86a0b439e3a52f5bdd4487ac98f3be9d93bdda
Tatyana Syrbu <tanja.karamilja@gmail.com>
2023-02-05 01:21:17 +0200

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,27 +1,34 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const { ctrlWrapper } = require("../../helpers");
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/", ctrlWrapper(ctrl.getAll));
+router.get("/", authenticate, ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getById));
+router.get("/:contactId", authenticate, ctrlWrapper(ctrl.getById));
 
-router.post("/", validateBody(schemas.addScheme), ctrlWrapper(ctrl.add));
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addScheme),
+  ctrlWrapper(ctrl.add)
+);
 
-router.delete("/:contactId", ctrlWrapper(ctrl.deleteById));
+router.delete("/:contactId", authenticate, ctrlWrapper(ctrl.deleteById));
 
 router.put(
   "/:contactId",
+  authenticate,
   validateBody(schemas.addScheme),
   ctrlWrapper(ctrl.updateById)
 );
 
 router.patch(
   "/:contactId/favorite",
+  authenticate,
   validateBody(schemas.updateFavoriteScheme),
   ctrlWrapper(ctrl.updateFavorit)
 );
